b4104aebf939cdb243389fe485c2578ddfdf29b1
N1krosRU <milksorc@gmail.com>
2023-01-04 18:40:37 +0400

    commi2

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('N1kros_1')
+    print_hi('N1kros_2')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
